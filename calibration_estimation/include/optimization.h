f79b827f12afdfd61f0492e7a296102fd8197e01
Pablo Speciale <pablo.speciale@gmail.com>
2013-05-20 01:47:24 -0700

    Added getCameraModels, getMeasurements, findCbPoses, getFrameNames

--- calibration_estimation/include/optimization.h
+++ calibration_estimation/include/optimization.h
@@ -45,6 +45,7 @@
 #include "chessboard.h"
 #include "conversion.h"
 #include "joint_state.h"
+#include "markers.h"
 #include "projection.h"
 #include "robot_state.h"
 #include "robot_state_publisher.h"
@@ -55,6 +56,27 @@ namespace calib
 
 class RobotState;
 
+struct View
+{
+  typedef std::vector<cv::Point3d> Points3D;
+  typedef std::vector<cv::Point2d> Points2D;
+
+  unsigned id;
+
+  Points3D              board_model_pts_3D;  // generateCorners
+  std::vector<Points2D> measured_pts_2D;     // getMeasurement
+  std::vector<Points2D> expected_pts_2D;     // findCbPoses
+  std::vector<double>   error;               // findCbPoses
+
+  std::vector<image_geometry::PinholeCameraModel> cam_model;
+//   unsigned cam_number;  cam_model.size()
+
+  std::vector<std::string>           frame_name;
+  std::map<std::string, std::size_t> frame_id;   // frame_name -> frame_id
+
+  std::vector<KDL::Frame> pose_rel, pose_father;
+  KDL::Frame T0; // T0 == pose_father[0]*pose_rel[0]
+};
 
 class Optimization
 {
@@ -64,9 +86,11 @@ public:
   Optimization();
   ~Optimization();
 
+  // Set optimizer funtions
   void setRobotState(RobotState *robot_state);
+  void setBagData(std::vector<Msg> *msgs);
+  void setMarkers(Markers *markers);
 
-  void setBagData(std::vector<Msg> &msgs);
 
   // addData
   // deleteData
@@ -74,6 +98,8 @@ public:
   void addMeasurement(Msg &msg);
 
 //   generateView();
+
+  bool valid();
 
   void run();
 
@@ -85,15 +111,43 @@ public:
 
 protected:
   // TODO: read this information from the system.yaml
-  void getCheckboardSize(const std::string &target_id, ChessBoard *cb);
+  void getCheckboardSize(const std::string &target_id,
+                         ChessBoard *cb);
 
   /// \brief Generate 3D chessboard corners (board_points)
-  void generateCorners(Msg &msg, std::vector<cv::Point3d> *board_model_pts_3D);
+  void generateCorners(const Msg &msg,
+                       std::vector<cv::Point3d> *board_model_pts_3D);
+
+  /// \brief Get Camera Model from Message
+  void getCameraModels(const Msg &msg,
+                      std::vector<image_geometry::PinholeCameraModel> *cams);
+
+
+  /// \brief Read image points from message
+  void getMeasurements(const Msg &msg,
+                       std::vector<View::Points2D> *measurements);
+
+  /// \brief Find chessboard poses using solvePnP
+  void findCbPoses(const Msg &msg,
+                   const std::vector<cv::Point3d> &board_model_pts_3D,
+                   const std::vector<View::Points2D> &measured_pts_2D,
+                   const std::vector<cv::Mat> &intrinsicMatrix,
+                   std::vector<cv::Mat> *rvec,
+                   std::vector<cv::Mat> *tvec,
+                   std::vector<View::Points2D> *expected_pts_2D,
+                   std::vector<double> *error);
+
+
+  void getFrameNames(const Msg &msg,
+                     std::vector<std::string> *frame,
+                     std::map<std::string, std::size_t> *frame_id);
+
 
 
 private:
-  RobotState *robot_state_;
-  std::vector<Msg> msgs_;
+  RobotState        *robot_state_;
+  std::vector<Msg>  *msgs_;
+  Markers           *markers_;
 };
 
 }
